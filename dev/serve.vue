b511e89994a6dce4fd15eb5b6bfe054046228814
nuwasdzarrin <nuwasdzarrin3@gmail.com>
2022-11-25 14:05:18 +0700

    update library

---
+++ dev/serve.vue
@@ -0,0 +1,19 @@
+<script>
+import Vue from 'vue';
+import VueElementHighlight from '@/vue-element-highlight.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueElementHighlight
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-element-highlight :is-active="true">
+      <span>Feature</span>
+    </vue-element-highlight>
+  </div>
+</template>
